444f57893c815cf20e8de70aad16232c2fe50e70
Cassandra Terry <cassandra34terry@gmail.com>
2020-02-27 22:53:52 -0600

    path change

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -51,16 +51,16 @@ export default class PreloadScene extends Phaser.Scene {
         });
 
     //Landing page
-        this.load.audio('justKeepSwimming', './src/assets/Dory.mp3');
-        this.load.audio('underwater', "./src/assets/underthesea.mp3");
-        this.load.image('bg', "./src/assets/deepblue.jpg");
-        this.load.image('sharkRight', "./src/assets/sharkRight.png");
-        this.load.image('fishRight', "./src/assets/fishRight.png");
+        this.load.audio('justKeepSwimming', 'src/assets/Dory.mp3');
+        this.load.audio('underwater', "src/assets/underthesea.mp3");
+        this.load.image('bg', "src/assets/deepblue.jpg");
+        this.load.image('sharkRight', "src/assets/sharkRight.png");
+        this.load.image('fishRight', "src/assets/fishRight.png");
 
     //Menu page
-        this.load.image('bg', "./src/assets/deepblue.jpg");
-        this.load.image('sharkLeft', "./src/assets/sharkTrim.png");
-        this.load.image('fishLeft', "./src/assets/fishLeft.png");
+        this.load.image('bg', "src/assets/deepblue.jpg");
+        this.load.image('sharkLeft', "src/assets/sharkTrim.png");
+        this.load.image('fishLeft', "src/assets/fishLeft.png");
 
 
     //Play page
@@ -68,22 +68,22 @@ export default class PreloadScene extends Phaser.Scene {
          this.load.audio('sharkHit', "./src/assets/hit.mp3");
 
          //images
-         this.load.image('red', "./src/assets/dripBlood.png");
-         this.load.image('ocean', "./src/assets/deepblue.jpg");
-         this.load.image('sharkFaceLeft', "./src/assets/sharkTrim.png");
-         this.load.image('sharkRight', "./src/assets/sharkRight.png");
-         this.load.image('bubbleParticle', "./src/assets/bubble.png");
-         this.load.spritesheet('fish', "./src/assets/spritesheet.png", {
+         this.load.image('red', "src/assets/dripBlood.png");
+         this.load.image('ocean', "src/assets/deepblue.jpg");
+         this.load.image('sharkFaceLeft', "src/assets/sharkTrim.png");
+         this.load.image('sharkRight', "src/assets/sharkRight.png");
+         this.load.image('bubbleParticle', "src/assets/bubble.png");
+         this.load.spritesheet('fish', "src/assets/spritesheet.png", {
              frameWidth: 480,
              frameHeight: 270
          });
-         this.load.spritesheet('collideLeft', "./src/assets/collideLeft.png", {
+         this.load.spritesheet('collideLeft', "src/assets/collideLeft.png", {
              frameWidth: 480,
              frameHeight: 270
          });
 
     //How To page
-         this.load.image('bg', "./src/assets/deepblue.jpg");
+         this.load.image('bg', "src/assets/deepblue.jpg");
 
     }
 
